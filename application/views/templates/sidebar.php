1bf88a898de994de8bba70de8f2115b86643b72c
Khairul Badri <khbadri12@gmail.com>
2025-08-06 22:56:10 +0700

    domain

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -35,7 +35,6 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="<?= base_url('dashboard') ?>" class="brand-link">
-        <img src="<?= base_url('assets/template/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-bold" style="color:#F3F7EC;">FreeRADIUS Billing </span>
       </a>
       <!-- Sidebar -->
