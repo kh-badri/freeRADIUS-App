0b0f438e0c17faaf51e5478e86d9c262590a93d5
Khairul Badri <khbadri12@gmail.com>
2025-08-07 14:20:08 +0700

    footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -6,7 +6,7 @@
 
 <footer class="main-footer">
   <div class="float-right d-none d-sm-block">
-    <b>Version</b> 3.0.5
+    <b>Khairul Badri</b>
   </div>
   <strong>Copyright &copy; 2025 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
   reserved.
